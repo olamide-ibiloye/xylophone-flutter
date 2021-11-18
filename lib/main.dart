e083ce1b86a323f268c2abdd22a94df6ed0d334f
Angela <8798027+angelabauer@users.noreply.github.com>
2021-11-18 17:15:26 +0000

    updating android and ios stub

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,58 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playTune(int soundNumber) {
+    AssetsAudioPlayer.newPlayer().open(
+      Audio("assets/note$soundNumber.wav"),
+    );
+  }
+
+  Expanded buildKey(Color keyColor, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          minimumSize: Size.zero,
+          padding: EdgeInsets.zero,
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        ),
+        child: Container(
+            color: keyColor,
+            child: null
+        ),
+        onPressed: () {
+          playTune(soundNumber);
+        },
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
